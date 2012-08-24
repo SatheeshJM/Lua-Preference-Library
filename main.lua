3987e2367ef4d203e89d670e65daf7e257b4882b
Satheesh Kumar <satheeshrulzz@gmail.com>
2012-08-25 04:26:32 +0530

    Initial Commit

--- main.lua
+++ main.lua
@@ -1,14 +1,21 @@
 local preference = require "preference"
-
-
-preference.save{a=1}
-value = preference.getValue("a")
  
+--Store numbers
+preference.save{a=1}
+value = preference.getValue("a
+print("Retrieving number : ",value)
+ 
+--Store strings
 preference.save{b="1"}
 value = preference.getValue("b")
+print("Retrieving string : ",value)
  
+--Store Boolean
 preference.save{c=true}
 value = preference.getValue("c")
+print("Retrieving boolean : ",value)
  
+--Store Tables
 preference.save{d = {1,"2",true}}
 value = preference.getValue("d")
+print("Retrieving table : ",value)
